22e75c9e0b8b1c4f07ee8da16f34ec72de38eef8
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-02 18:48:32 +0300

    add views for home page and all-post pages

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def start_page(request):
+    return render(request, 'blog/blog-home.html')
+
+def all_post_page(request):
+    return render(request, 'blog/all-post.html')
