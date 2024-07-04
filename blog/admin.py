e32f9077d615e1e66b0e833a2c84f665ec63f5b2
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-04 18:48:18 +0300

    make slug auto populated using title

--- blog/admin.py
+++ blog/admin.py
@@ -3,6 +3,14 @@ from .models import Post, Comment, Like
 
 # Register your models here.
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    list_filter = ('date',)
+    prepopulated_fields = {
+        'slug' : ('title',)
+    }
+
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
 admin.site.register(Like)
