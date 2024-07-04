7e47cd1c63c06aed2e6aec2ad80dad2aafca2b5b
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-04 19:14:04 +0300

    add post detail view to url path

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.StartPageView.as_view(), name='home-page'),
     path('samuel-all-posts', views.AllPostPageView.as_view(), name='all-posts'),
+    path('post/<slug:slug>', views.PostDetailPage.as_view(), name='post-detail')
 ]
