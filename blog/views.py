cffce08c88b59bf111248b9c3ebaefc63527d65c
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-09 13:51:26 +0300

    make read later view for get and post request

--- blog/views.py
+++ blog/views.py
@@ -30,6 +30,12 @@ class AllPostPageView(ListView):
 
 
 class PostDetailPage(View):
+    def is_stored(self,request, post):
+        stored_post = request.session.get('stored-post')
+        if stored_post == None or len(stored_post) == 0:
+            return False
+        return post in stored_post
+    
     def get(self, request, slug):
         post= Post.objects.get(slug=slug)
         comments = Comment.objects.filter(post=post.id)
@@ -38,7 +44,8 @@ class PostDetailPage(View):
         return render(request, 'blog/post-detail.html', {
             'post' : post,
             'comments':comments,
-            'form' : form
+            'form' : form,
+            'saved_for_later' : self.is_stored(request, int(post.id))
         })
     
     def post(self, request, slug):
@@ -56,7 +63,41 @@ class PostDetailPage(View):
         context = {
             'post' : post,
             'comments':comments,
-            'form' : comment_form
+            'form' : comment_form,
+            'saved_for_later' : self.is_stored(request, int(post.id))
         }
         
         return render(request, 'blog/post-detail.html', context)
+    
+    
+class StoredPostView(View):
+    def get(self, request):
+        stored_posts = request.session.get('stored-post')
+        context = {}
+        
+        if stored_posts is None or len(stored_posts) == 0:
+            context['posts'] = []
+            context['has_post'] = False
+        else:
+            context['stored_posts'] = Post.objects.filter(id__in=stored_posts)
+            context['has_post'] = True
+        
+        return render(request, 'blog/stored-post.html', context)
+    
+    def post(self, request):
+        stored_posts = request.session.get('stored-post')
+        
+        if stored_posts == None or len(stored_posts) == 0:
+            stored_posts = []
+        
+        post_id = int(request.POST.get('post-id'))
+        
+        if post_id not in stored_posts:
+            stored_posts.append(post_id)
+        else:
+            stored_posts.remove(post_id)
+        
+        request.session['stored-post'] = stored_posts
+        
+        detail_page = request.META.get('HTTP_REFERER')
+        return HttpResponseRedirect(detail_page)
