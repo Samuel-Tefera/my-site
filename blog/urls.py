2356dc41fee898a559a814c24735978276bdd4a0
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-09 13:50:37 +0300

    set up url for read later feature

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.StartPageView.as_view(), name='home-page'),
     path('samuel-all-posts', views.AllPostPageView.as_view(), name='all-posts'),
-    path('post/<slug:slug>', views.PostDetailPage.as_view(), name='post-detail')
+    path('post/<slug:slug>', views.PostDetailPage.as_view(), name='post-detail'),
+    path('stored-post', views.StoredPostView.as_view(), name='stored-post')
 ]
