e2615fccc58d799f6f098be912517151dc3c9fa1
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 14:28:44 +0300

    replace secret key and debug by enviroment variable

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jvvo04zcn_u)+9k0jq*z^dqd9ect7)u0&5lj3l868auzvnovz$'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('IS_DEVELOPMENT', False)
 
 ALLOWED_HOSTS = []
 
